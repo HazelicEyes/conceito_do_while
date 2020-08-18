b2547101405474d159d16e73b721e261d4583121
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-18 03:59:51 -0300

    Add project files.

---
+++ conceito_do_while/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conceito_do_while
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            do
+            {
+                c = c + 1;
+                lblNumero.Text = Convert.ToString(c);
+                lblNumero.Refresh();
+            }
+            while (c < 10000);
+            // sempre executa pois a condicao do vem no final, so é utilizado
+            // quando é necessario testar a condicao depois
+        }
+    }
+}
